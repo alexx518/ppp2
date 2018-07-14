9cae8d3edfe971663b314fcdace6f88749c9924b
ar <alexx518@users.noreply.github.com>
2018-07-14 22:22:41 +0200

    init

---
+++ main.cpp
@@ -0,0 +1,20 @@
+#include "Simple_window.h"
+#include "Graph.h"
+
+int main() {
+    using namespace Graph_lib;
+    Point tl{200,200};
+    
+    Simple_window win {tl,600,400,"test"};
+    
+    Polygon poly;
+    
+    poly.add(Point{300,200});
+    poly.add(Point{350,100});
+    poly.add(Point{400,200});
+    
+    poly.set_color(Color::red);
+    win.attach (poly);
+    win.wait_for_button();
+}
+
